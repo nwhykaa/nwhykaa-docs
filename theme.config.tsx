cc4fd3c24661a19054c1cd3cd0c8ce2462d0c6b5
ykaa <pepahurka3@gmail.com>
2026-03-14 23:36:10 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,28 @@
 export default {
-  logo: <strong style={{ fontSize: '1.25rem' }}>YKAA SCRIPTS</strong>,
+  logo: <span style={{ fontWeight: 800, letterSpacing: '-0.5px' }}>XKAA SCRIPTS</span>,
   project: {
     link: 'https://github.com/nwhykaa',
   },
   docsRepositoryBase: 'https://github.com/nwhykaa/nwhykaadocs/tree/main',
+  nextThemes: {
+    defaultTheme: 'dark',
+  },
+  toc: {
+    component: null
+  },
   footer: {
     text: (
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', fontSize: '0.8rem' }}>
-        <span>Build by nwhykaa. The source code is available on GitHub.</span>
+      <div className="flex w-full items-center justify-between text-sm text-gray-400">
+        <div>
+          <span className="mr-2">⌘</span> Build by <a href="#" className="underline">nwhykaa</a>. The source code is available on <a href="#" className="underline">GitHub</a>.
+        </div>
       </div>
-    ),
+    )
   },
-  // Tohle schová pravý panel a vyhledávání na úvodní stránce
-  feedback: { content: null },
-  editLink: { component: null },
-  toc: { component: null }
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  }
 }
