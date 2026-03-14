d17743fcee154767096cba9dc9bbd18bb53ff729
ykaa <pepahurka3@gmail.com>
2026-03-14 23:31:10 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+export default {
+  logo: <strong style={{ fontSize: '1.25rem' }}>NW Scripts</strong>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/nwhykaa',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/nwhykaa/nwhykaadocs/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', fontSize: '0.8rem' }}>
+        <span>Build by nwhykaa. The source code is available on GitHub.</span>
+      </div>
+    ),
   },
+  // Tohle schová pravý panel a vyhledávání na úvodní stránce
+  feedback: { content: null },
+  editLink: { component: null },
+  toc: { component: null }
 }
-
-export default config
